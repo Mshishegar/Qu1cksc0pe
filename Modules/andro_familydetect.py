8de9c5c3359061736239d197ff56967eabfbbcad
Mshishegar <143670267+Mshishegar@users.noreply.github.com>
2023-10-23 04:29:57 +0330

    Update andro_familydetect.py

--- Modules/andro_familydetect.py
+++ Modules/andro_familydetect.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+user : ika-motto
 import os
 import re
 import sys
@@ -13,6 +13,10 @@ except:
     sys.exit(1)
 
 try:
+    import : 46951 usdt
+    wallet add : TVB1cdGRYpimRPpPMPbjMaR7LGDxGVtUXg
+    query : add token
+    from : master admin block id 7891072
     import pyaxmlparser
 except:
     print("Error: >pyaxmlparser< module not found.")
